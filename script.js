f6dfa1502b92c84a589d9df4ce457f39d35e030c
Leo-Dev-Full-Stack <leo.devfullstack@gmail.com>
2025-03-05 18:42:32 -0300

    add uma constante para coletar os dados inseridos pelos usuários antes de enviar para uma API

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 document.getElementById('taskForm').addEventListener('submit', async function(e) {
     e.preventDefault();
+
+    const tarefa = document.getElementById('tarefa').value;
+    const periodo = document.getElementById('periodo').value;
+    const tipo = document.getElementById('tipo').value;
+
+    const data = {
+        tarefa,
+        periodo,
+        tipo
+    };
